9445c55a93f1a8ad25b8002466c4f0efe0e054db
miruetoto <miruetoto@gmail.com>
2021-09-19 13:20:57 +0900

    .

--- _notebooks/heavysnow.py
+++ _notebooks/heavysnow.py
@@ -1,4 +1,5 @@
 import numpy as np
+from plotnine import * 
 
 class GraphSignal: 
     def __init__(self,V,W,f):
@@ -9,7 +10,7 @@ class GraphSignal:
         self.degree=np.sum(np.array(self.W),0)
         self.initdist=self.degree/np.sum(self.degree) 
         
-class HeavySnowTransform:  
+class HeavysnowTransform:  
     def __init__(self,graphsignal):
         self.n=graphsignal.n
         self.f=graphsignal.f
@@ -20,8 +21,8 @@ class HeavySnowTransform:
         self._initialize()
     
     def _initialize(self):
-        self.tau=0
-        self.b=0
+        self.tau=None
+        self.b=None
         self.trajectory=[np.random.choice(self.n,1,p=self.initdist).item()]
         self.flowcount=[0]
         self.snowygrounds=None 
@@ -29,6 +30,7 @@ class HeavySnowTransform:
         self.snowdistance=np.zeros((self.n,self.n))
         self.euclidweight=np.zeros((self.n,self.n))
         self.snowweight=np.zeros((self.n,self.n))
+        self.theta=None
         
     def _snowonce(self,ell,maxflow):
         b=self.b
@@ -70,17 +72,18 @@ class HeavySnowTransform:
         return np.sqrt(self._getdegreematrix(matrix))@matrix@np.sqrt(self._getdegreematrix(matrix))
     
     def _updateeuclidweight(self):
-        self.euclidweight=np.exp(-self.eucliddistance/(self.b**2))-np.eye(self.n)
+        self.euclidweight=np.exp(-self.eucliddistance/(2*self.theta**2))-np.eye(self.n)
         
     def _updatesnowdistance(self):
         self.snowdistance=np.sum((self.snowygrounds[:,np.newaxis,:]-self.snowygrounds[np.newaxis,:,:])**2,axis=-1)
              
     def _updatesnowweight(self):
-        self.snowweight=np.exp(-self.snowdistance/(self.tau*self.b**2))-np.eye(self.n)
+        self.snowweight=np.exp(-self.snowdistance/(self.tau*2*self.theta**2))-np.eye(self.n)
     
     def snow(self,tau,b=1,maxflow=100000):
         self._initialize()
         self.b=b
+        self.theta=np.sqrt(1/2)*self.b
         self.tau=tau
         self.snowygrounds=np.repeat(self.f,self.tau+1).reshape(self.n,self.tau+1)
         print('HST (tau= %s, b=%s)' % (self.tau,self.b))
@@ -92,100 +95,68 @@ class HeavySnowTransform:
         self._updatesnowweight()
         self._updateeuclidweight()                
         
-# class HeavySnowTransform_old:  #hst(f,W,V,τ=τ,b=0.03,γ=1)
-#     def __init__(self,graphsignal):
-#         self.n=graphsignal.n
-#         self.f=graphsignal.f
-#         self.V=graphsignal.V
-#         self.graphweight=graphsignal.W
-#         #self.degree=graphsignal.degree
-#         self.initdist=graphsignal.initdist
-#         self._initialize()
-    
-#     def _initialize(self):
-#         self.tau=0
-#         self.b=0
-#         self.trajectory=[0]
-#         self.flowcount=[0]
-#         self.snowygrounds=pd.DataFrame(data={'h0':self.f})            
-#         self.snowygrounds_f=pd.DataFrame(data={'h0':self.f})            
-#         self.snowygrounds_sumxi=pd.DataFrame(data={'h0':self.f*0})            
-#         self.eucliddistance=(np.array(self.f)[:,np.newaxis]-np.array(self.f)[np.newaxis,:])**2
-#         self.snowdistance=np.zeros((self.n,self.n))
-# #         self.snowdistance1=np.zeros((self.n,self.n))
-# #         self.snowdistance2=np.zeros((self.n,self.n))
-# #         self.snowdistance3=np.zeros((self.n,self.n))
-#         self.euclidweight=np.zeros((self.n,self.n))
-#         self.snowweight=np.zeros((self.n,self.n))
-        
-#     def _snowonce(self,ell,b,maxflow):
-#         W=self.graphweight>np.asmatrix(np.random.random((self.n,self.n)))
-#         hnext=np.array(self.snowygrounds.iloc[:,-1])
-#         currentnode=self.trajectory[-1]
-#         flowcount=self.flowcount[-1]
-#         # 1. h(u) <- h(u)+b : update current node when flowcound > 0 
-#         hnext[currentnode]=hnext[currentnode]+b
-#         # 2. check that: are there any nodes to which snow can flow from u. 
-#         neighbor=np.where(W[currentnode,:]>0)[1] ## Nu is np.array
-#         if len(neighbor)==0: downstream=np.array([])
-#         else: downstream=neighbor[list((np.where(hnext[neighbor]<=hnext[currentnode]))[0])]
-#         # 3. check flowable 
-#         flowable = len(downstream)>0 and flowcount < maxflow 
-#         # 4. determine flow or block
-#         if flowable==0: # block!
-#             node0=np.random.choice(self.n,1,p=self.initdist).item()
-#             neighbor=np.where(W[node0,:]>0)[1] 
-#             downstream=neighbor[list((np.where(hnext[neighbor]<=hnext[node0]))[0])]
-#             if len(downstream)==0: 
-#                 nextnode=node0
-#             else: 
-#                 nextnode=np.random.choice(list(downstream),1).item()
-#             flowcount=0
-#         else: #flow
-#             nextnode=np.random.choice(list(downstream),1).item()
-#             flowcount=flowcount+1
-#         self.snowygrounds['h%s' % ell]=hnext # pd.concat 
-#         self.flowcount=self.flowcount+[flowcount] #pd.concat 
-#         self.trajectory=self.trajectory+[nextnode] #pd.concat 
-        
-#         self.snowygrounds_f['h%s' % ell]=self.f
-#         self.snowygrounds_sumxi['h%s' % ell]=hnext-self.f
+    def adjustingtheta(self,theta): 
+        self.theta=theta
+        self._updatesnowweight()
+        self._updateeuclidweight()                
 
-#     def _getdegreematrix(self,matrix):
-#         return np.diag(np.sum(matrix,1))
+class SpectralAnalysis:        
+    def __init__(self,graphsignal):
+        self.f=graphsignal.f
+        self.V=graphsignal.V
+        self.W=graphsignal.W
+        self.n=graphsignal.n
+        self.degree=graphsignal.degree        
+        self._initialize()
     
-#     def _normalize(self,matrix): 
-#         return np.sqrt(self._getdegreematrix(matrix))@matrix@np.sqrt(self._getdegreematrix(matrix))
+    def _initialize(self):
+        self.D=np.diag(self.degree)
+        self.L=self.D-self.W
+        _drootinv=np.zeros_like(self.degree)
+        _drootinv[self.degree>0]= 1/np.sqrt(self.degree[self.degree>0])
+        self.Lz=np.diag(_drootinv)@(self.L)@np.diag(_drootinv)
+        self.Psi= None #
+        self.lamb= None
+        self.Lamb= None
+        self.fbar= None
+        self.components=None
     
-#     def _updateeuclidweight(self):
-#         self.euclidweight=np.exp(-self.eucliddistance/(self.b**2))-np.eye(self.n)
+    def graphFouriertransform(self):
+        self.Psi,self.lamb,_ = np.linalg.svd(self.Lz)
+        self.Lamb = np.diag(self.lamb)
+        self.fbar = self.Psi.T @ self.f 
         
-#     def _updatesnowdistance(self):
-#         self.snowdistance=np.sum((np.array(self.snowygrounds)[:,np.newaxis,:]-np.array(self.snowygrounds)[np.newaxis,:,:])**2,axis=-1)
-        
-# #     def _updatesnowdistance1(self):
-# #         self.snowdistance1=np.sum((np.array(self.snowygrounds_f)[:,np.newaxis,:]-np.array(self.snowygrounds_f)[np.newaxis,:,:])**2,axis=-1)
-        
-# #     def _updatesnowdistance2(self):
-# #         self.snowdistance2=np.sum(2*(np.array(self.snowygrounds_f)[:,np.newaxis,:]-np.array(self.snowygrounds_f)[np.newaxis,:,:])*(np.array(self.snowygrounds_sumxi)[:,np.newaxis,:]-np.array(self.snowygrounds_sumxi)[np.newaxis,:,:])   ,axis=-1)
+    def decompose(self):
+        _complist = [(np.outer(self.Psi[i],self.Psi[i])@self.f).tolist() for i in range(self.n)] 
+        self.components = np.array(_complist).T
 
-# #     def _updatesnowdistance3(self):
-# #         self.snowdistance3=np.sum((np.array(self.snowygrounds_sumxi)[:,np.newaxis,:]-np.array(self.snowygrounds_sumxi)[np.newaxis,:,:])**2,axis=-1)
+    def _specplot(self):
+        thm = theme_classic()
+        aes1= aes(x=self.lamb,y=np.abs(self.fbar),xend=self.lamb,yend=0)
+        aes2= aes(x=self.lamb,y=np.abs(self.fbar))
+        g1= geom_segment(aes1)  
+        g2= geom_point(aes2)    
+        fig = ggplot()+thm+g1+g2+xlim(0,2.1)+ xlab("")+ ylab("") + ggtitle("Periodogram")
+        return fig 
+    
+    def _decomplot(self):       
+#         ggplot(data=tibble(V=V,f=f,Vtext=Vtext),aes(x=V,y=f,label=Vtext))+
+#         geom_col(aes(fill=(f>0)),width=0.1)+geom_hline(aes(yintercept=0),col="gray60",lty=2)+
+#         geom_text(fontface = 4,size=8)+
+#         xlab("")+ylab("")+guides(fill=FALSE)+theme(plot.title=element_text(face="bold.italic"))+
+#         theme_bw()+
+#         theme(strip.text.x = element_text(size = 20, color = "black", face = "bold.italic"))+
+#         theme(strip.text.y = element_text(size = 15, color = "black", face = "bold.italic"))+
+#         ylim(-1.2,1.2)+
+#         theme(plot.title=element_text(face="bold.italic"))
         
-#     def _updatesnowweight(self):
-#          self.snowweight=np.exp(-self.snowdistance/(self.tau*self.b**2))-np.eye(self.n)
-#     def snow(self,tau,b=1,maxflow=100):
-#         self._initialize()
-#         self.b=b
-#         self.tau=tau
-#         print('HST (tau= %s, b=%s)' % (tau,b))
-#         for ell in np.arange(1,tau+1): 
-#             print('\r'+str(ell)+'/'+str(tau),sep='',end='')
-#             self._snowonce(ell,b,maxflow)
-#         print('\n'+'HST completed and all history is recorded.')
-#         self._updatesnowdistance()
-#         self._updatesnowdistance1()
-# #        self._updatesnowdistance2()
-# #        self._updatesnowdistance3()
-# #        self._updatesnowweight()
-#         self._updateeuclidweight()
+        
+        df=
+        thm = theme_classic()
+        aes1= aes(x=self.V,y=np.abs(self.fbar),xend=self.lamb,yend=0)
+        aes2= aes(x=self.lamb,y=np.abs(self.fbar))
+        g1= geom_col(aes1)  
+        g2= geom_text(aes2)    
+        fig = ggplot()+thm+g1+g2+xlim(0,2.1)+ xlab("")+ ylab("") + ggtitle("Periodogram")
+        return fig 
+        
