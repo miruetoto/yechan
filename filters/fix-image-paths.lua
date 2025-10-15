b47cdc72cdfe4782a65a74292d6fd6f8e1950875
miruetoto <miruetoto@gmail.com>
2025-10-15 12:51:22 +0900

    .

--- filters/fix-image-paths.lua
+++
@@ -1,6 +0,0 @@
-function Image(elem)
-  if elem.src:match("^%./첨부파일들/") then
-    elem.src = elem.src:gsub("^%./첨부파일들/", "https://raw.githubusercontent.com/miruetoto/yechan/main/posts/Yechan-md/첨부파일들/")
-  end
-  return elem
-end
--- posts/Yechan-md/template/template.md
+++
@@ -1,8 +0,0 @@
----
-title: (분류) 제목
-author: 신록예찬
-date: 01/01/2099
-draft: false
----
-
-[[Yechan 인덱스]]
