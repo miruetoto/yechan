37da226ec785c8ba7f1bc38e5254bb0ebd4d2650
miruetoto <miruetoto@gmail.com>
2026-04-11 18:46:08 +0900

    .

--- pub.sh
+++ pub.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# 원격 서버에서 실행
+sshpass -p '123qwe' ssh -p 43052 root@210.117.173.186 << 'ENDSSH'
+cd ~/Dropbox/01-rsch/9999-Yechan
+
 # 이미지 참조를 URL 인코딩 형식으로 변환 (옵시디언 호환성)
 uv run python url_encode_images.py
 
@@ -14,3 +18,4 @@ quarto render
 git add -A
 git commit -m "."
 git push origin main
+ENDSSH
