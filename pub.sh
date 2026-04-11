58c2453d0ef84f8a7efa7f68b61b84380abb38cd
miruetoto <miruetoto@gmail.com>
2026-04-11 19:50:26 +0900

    .

--- pub.sh
+++ pub.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 원격 서버에서 실행
-sshpass -p '123qwe' ssh -p 43052 root@210.117.173.186 << 'ENDSSH'
+sshpass -p '123qwe' ssh -tt -p 43052 root@210.117.173.186 << 'ENDSSH'
 cd ~/Dropbox/01-rsch/9999-Yechan
 
 # 이미지 참조를 URL 인코딩 형식으로 변환 (옵시디언 호환성)
